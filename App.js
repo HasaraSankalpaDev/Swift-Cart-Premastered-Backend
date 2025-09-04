b22d3e996ae4bbd39f1a4a78d89d12a25b4a0521
HasaraSankalpaDev <hasarasankalpa176@gmail.com>
2025-09-04 22:32:13 +0530

    changed port

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ mongoose
   )
   .then(() => {
     console.log("Connected to MongoDB");
-    const PORT = process.env.PORT || 8000;
+    const PORT = process.env.PORT || 8080;
 
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
